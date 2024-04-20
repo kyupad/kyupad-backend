9427b7968cea459b91e974775a2ca15a74fbe70d
LongLD <longld@fabbi.com.vn>
2024-04-21 03:21:33 +0700

    refactor(backend): allow devnet env

--- src/validation/env.validation.ts
+++ src/validation/env.validation.ts
@@ -5,7 +5,9 @@ const envSchema = Joi.object({
   BASE_URL: Joi.string().required(),
   NODE_ENV: Joi.string().valid('development', 'production', 'test'),
   WEB_URL: Joi.string().required(),
-  CHAIN_ID: Joi.string().valid(CHAIN_ID.Mainnet, CHAIN_ID.Testnet).required(),
+  CHAIN_ID: Joi.string()
+    .valid(CHAIN_ID.Mainnet, CHAIN_ID.Testnet, CHAIN_ID.Devnet)
+    .required(),
   JWT_ACCESS_TOKEN_SECRET: Joi.string().required(),
   JWT_REFRESH_TOKEN_SECRET: Joi.string().required(),
   MONGODB_URI: Joi.string().required(),
